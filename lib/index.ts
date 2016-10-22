c9f99254c2b5a0e65722be45a15a9aa56aa7378e
srinivas.dasari <srinivas.dasari@fireeye.com>
2016-10-22 18:15:02 +0530

    added module

---
+++ lib/index.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { Ng2GifPreview } from './ng2GifPreview';
+
+@NgModule({
+  declarations: [Ng2GifPreview],
+  exports: [Ng2GifPreview]
+})
+export class Ng2GifPreviewModule {}
